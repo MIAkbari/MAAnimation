2308567add0ae95343a35feff39511130e4b672d
Mohammad <ma@bornak.de>
2021-11-23 11:56:05 +0330

    Initial Commit

---
+++ Tests/MAAnimationTests/MAAnimationTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MAAnimation
+
+final class MAAnimationTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MAAnimation().text, "Hello, World!")
+    }
+}
