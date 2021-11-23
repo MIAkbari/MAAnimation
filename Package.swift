74fba8e936d9d7aa879d7601769dc888d2b93480
Mohammad <ma@bornak.de>
2021-11-23 14:35:46 +0330

    Remove Test

--- Package.swift
+++ Package.swift
@@ -23,8 +23,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "MAAnimation",
             dependencies: []),
-        .target(name: "RxTestProject",
-                dependencies: ["RxSwift", "RxCocoa"])
     ]
     
 )
